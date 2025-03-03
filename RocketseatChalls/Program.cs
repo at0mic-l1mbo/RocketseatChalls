2f07afb8db10d14b979caa1f0f89952c410bec3b
at0mic-l1mbo <augustolazer@gmail.com>
2025-03-02 21:41:11 -0300

    Add project files.

---
+++ RocketseatChalls/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace RocketseatChalls;
+
+class Program
+{
+    public static void Main()
+    {
+        string name;
+        do {
+            Console.WriteLine("Digite aqui o seu nome: ");
+            name = Console.ReadLine();  
+            if(name == String.Empty)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("[-] O campo nome não pode ser vazio, tente novamente...\n");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        
+        } while (name == String.Empty);
+
+        Console.WriteLine($"Olá {name}! Seja muito bem-vindo!");
+    }
+}
